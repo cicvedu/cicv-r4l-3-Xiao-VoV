5b571f948feecb3ced10f7e9ab5ceedb4c966e9e
X.P <a1679190@gmail.com>
2024-07-21 10:25:04 +0800

    作业4

--- linux/rust/kernel/pci.rs
+++ linux/rust/kernel/pci.rs
@@ -327,6 +327,11 @@ impl Device {
     pub fn map_resource(&self, resource: &Resource, len: usize) -> Result<MappedResource> {
         MappedResource::try_new(resource.start, len)
     }
+
+    /// get pci point
+    pub fn get_pci_dev_ptr(&self) -> *mut bindings::pci_dev {
+        self.ptr
+    }
 }
 
 unsafe impl device::RawDevice for Device {
